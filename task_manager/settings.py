1476ca8313fe5d38aa5a153012a2dfd154a4340a
srx <srx92@protonmail.com>
2025-01-23 17:10:55 +0200

    modified settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
     "django-insecure-+o0$k(zg0&kq+ufys(87khjccdn3!vk%58bho5gk^5ntu7apmd"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -136,9 +138,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "manager.Worker"
 
-LOGIN_REDIRECT_URL  = "/manager/"
+LOGIN_REDIRECT_URL  = "/"
 
-LOGOUT_REDIRECT_URL = "/manager/"
+LOGOUT_REDIRECT_URL = "/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
